1986c15f417b48c598add75b136d0abf1bb80fd3
scruz <sebastian.cruz@wenance.com>
2021-04-13 17:43:55 -0300

    project creation

---
+++ src/main/java/org/ground/play/pluconference/PluconferenceApplication.java
@@ -0,0 +1,13 @@
+package org.ground.play.pluconference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluconferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluconferenceApplication.class, args);
+	}
+
+}
